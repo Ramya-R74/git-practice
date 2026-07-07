a87b8e2e72918d312fb7ff6bd36751b3ae07efd4
Ramya-R74 <nikkiramya74@gmail.com>
2026-07-07 17:10:46 +0530

    hint functionality

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import random
 def start_game():
     number_to_guess = random.randint(1, 10)
     print("Welcome to the Guessing Game!")
+    print("Hint: The number is between 1 and 10! but mostly the system select number 6 and 8.")
     guess = int(input("Guess a number between 1 and 10: "))
 
     if guess == number_to_guess:
