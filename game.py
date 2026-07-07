f33a917acbc35b4c242669d23d78590108de4495
Ramya-R74 <nikkiramya74@gmail.com>
2026-07-07 17:13:24 +0530

    Update welcome message

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ import random
 
 def start_game():
     number_to_guess = random.randint(1, 10)
-    print("Welcome to the Guessing Game!")
+    print("Welcome to the AMAZING Guessing Game!")
     guess = int(input("Guess a number between 1 and 10: "))
 
     if guess == number_to_guess:
